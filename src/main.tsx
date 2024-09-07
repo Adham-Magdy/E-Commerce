6ebe44a2a23ec31d8b2b1504be47a5c84cf17b91
Adham-Magdy <adhammagdy349@gmail.com>
2024-09-07 19:04:13 +0300

    Refactor: app routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import MainLayout from './layouts/MainLayout/MainLayout'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Categories from './pages/Categories';
-import Products from './pages/Products';
-import AboutUs from './pages/AboutUs';
+import AppRoutes from './routes/AppRoutes';
 
 
-// create main layout routes
-const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<MainLayout/>,
-        children:[
-            {
-                index:true,
-                element:<Home/>
-            },
-            {
-                path:"categories",
-                element:<Categories/>
-            },
-            {
-                path:"products/:prefix",
-                element:<Products/>
-            },
-            {
-                path:"about-us",
-                element:<AboutUs/>
-            }
-        ]
-    }
-]);
+
 
 createRoot(document.getElementById('root')!).render(
-<RouterProvider router={router}/>
+<AppRoutes/>
 
 )
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout/MainLayout';
+import AboutUs from '../pages/AboutUs';
+import Categories from '../pages/Categories';
+import Home from '../pages/Home';
+import Products from '../pages/Products';
+
+
+
+// create main layout routes
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<MainLayout/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+            {
+                path:"categories",
+                element:<Categories/>
+            },
+            {
+                path:"products/:prefix",
+                element:<Products/>
+            },
+            {
+                path:"about-us",
+                element:<AboutUs/>
+            }
+        ]
+    }
+]);
+const AppRoutes = () => {
+  return (
+   <RouterProvider router={router}/>
+  )
+}
+
+export default AppRoutes
