51acbcff419e8453cb2a16e9a547bc860853a295
Adham-Magdy <adhammagdy349@gmail.com>
2024-09-09 20:00:10 +0300

    feat: implement error page

---
+++ src/pages/Error.tsx
@@ -0,0 +1,21 @@
+import { Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
+const Error = () => {
+  return (
+    <Container>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      {/* Using replace method in link react dom to replace 
+      current page with previous page
+      */}
+      <Link to={'/'} replace={true}>
+      {" "}
+      Looks Like you have reached to non-existent page <br/>
+      How about going back to safety
+      </Link>
+    </Container>
+  )
+}
+
+export default Error
