f372878b051e9ae20ee5fa6f9ec07ebc3ca3eeef
Adham-Magdy <adhammagdy349@gmail.com>
2024-09-09 21:01:55 +0300

    feat: React router dynamic page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,11 +1,27 @@
 import { Container } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom"
 import "../styles/globalStyle.css"
-const Error = () => {
+const Error = () => { 
+    const error = useRouteError(); // hold status code , status error text
+    let errorStatus: number;
+    let errorStatusText: string;
+
+    /**
+     * check if error is react router error by using isRouteErrorResponse
+     */
+    if(isRouteErrorResponse(error)){
+        errorStatus = error.status;
+        errorStatusText = error.statusText;
+    }else{
+        errorStatus=404;
+        errorStatusText = "Page Not Found";
+    }
+
+
   return (
     <Container className="not_found">
-      <h1>404</h1>
-      <p>Page Not Found</p>
+      <h1>{errorStatus}</h1>
+      <p>{errorStatusText}</p>
       {/* Using replace method in link react dom to replace 
       current page with previous page
       */}
