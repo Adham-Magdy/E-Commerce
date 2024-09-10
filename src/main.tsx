88bad38c1dfc838d12e380f036ca4c40b41a78c0
Adham-Magdy <adhammagdy349@gmail.com>
2024-09-10 16:54:03 +0300

    feat: configure redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppRoutes from './routes/AppRoutes';
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppRoutes from "./routes/AppRoutes";
+import { Provider } from "react-redux";
+import store from "./store";
 
-
-
-
-createRoot(document.getElementById('root')!).render(
-<AppRoutes/>
-
-)
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <AppRoutes />
+  </Provider>
+);
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import categoriesSlice from "./categories/categoriesSlice";
+
+export const store = configureStore({
+    reducer:{
+        categories:categoriesSlice
+    }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
