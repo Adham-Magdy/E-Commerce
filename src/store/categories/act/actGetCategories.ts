fa5b2057378897c8ec9b871f56e5c437b93c9ecf
Adham-Magdy <adhammagdy349@gmail.com>
2024-09-10 17:12:08 +0300

    feat: create Async Thunk service for categories

---
+++ src/store/categories/act/actGetCategories.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// type of response
+type TResponse = {
+  id: number;
+  title: string;
+  prefix: string;
+  img: string;
+};
+const actGetCategories = createAsyncThunk(
+  "categories/actGetCategories",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    const url = "http://localhost:5005/categories";
+    try {
+      const response = await axios.get<TResponse>(url);
+      return response.data;
+    } catch (error) {
+      // in case of axios error
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error.response?.data.message);
+      } else {
+        return rejectWithValue("Unexpected Error");
+      }
+    }
+  }
+);
+
+export default actGetCategories;
