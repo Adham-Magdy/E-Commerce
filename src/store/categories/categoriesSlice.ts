43e9273dadea9942216c34dc456b5d6f9ee29997
Adham-Magdy <adhammagdy349@gmail.com>
2024-09-13 15:15:49 +0300

    feat: implement product slice reducer

--- src/store/categories/categoriesSlice.ts
+++ src/store/categories/categoriesSlice.ts
@@ -3,44 +3,41 @@ import actGetCategories from "./act/actGetCategories";
 import { TLoading } from "../../types/shared";
 import { TCategory } from "../../types/category";
 
-
-
-
-
 interface ICategoriesState {
-    records:TCategory[];
-    loading: TLoading;
-    error:string | null;
-};
+  records: TCategory[];
+  loading: TLoading;
+  error: string | null;
+}
 const initialState: ICategoriesState = {
-    records:[],
-    loading:"idle",
-    error:null
+  records: [],
+  loading: "idle",
+  error: null,
 };
 const categoriesSlice = createSlice({
-    name:"categories",
-    initialState,
-    reducers:{},
-    extraReducers:(builder)=>{
-        builder.addCase(actGetCategories.pending,(state)=>{
-            state.loading = "pending";
-            state.error = null;
-        }); // pending case
-        builder.addCase(actGetCategories.fulfilled,(state,action:PayloadAction<TCategory>)=>{
-            state.loading = "succeed";
-            state.records = action.payload;
-        });// fulfilled case
-        builder.addCase(actGetCategories.rejected,(state,action)=>{
-            state.loading="failed";
-            // set guard to ensure payload is string or null
-            if(action.payload && typeof action.payload ==="string"){
-                state.error = action.payload;
-
-            }
-        })
-    }
+  name: "categories",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(actGetCategories.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    }); // pending case
+    builder.addCase(
+      actGetCategories.fulfilled,
+      (state, action: PayloadAction<TCategory>) => {
+        state.loading = "succeed";
+        state.records = action.payload;
+      }
+    ); // fulfilled case
+    builder.addCase(actGetCategories.rejected, (state, action) => {
+      state.loading = "failed";
+      // set guard to ensure payload is string or null
+      if (action.payload && typeof action.payload === "string") {
+        state.error = action.payload;
+      }
+    });
+  },
 });
 
-
-export {actGetCategories};
+export { actGetCategories };
 export default categoriesSlice.reducer;
---
+++ src/store/products/productsSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import Products from "../../pages/Products";
+import { TCategory } from "../../types/category";
+import { TLoading } from "../../types/shared";
+import actGetProductsByCatPrefix from "./act/actGetProductsByCatPrefix";
+
+interface IProductsModel {
+  records: TCategory[];
+  loading: TLoading;
+  error: string | null;
+}
+
+const initialState: IProductsModel = {
+  records: [],
+  loading: "idle",
+  error: null,
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(actGetProductsByCatPrefix.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    }); // pending case
+    builder.addCase(
+      actGetProductsByCatPrefix.fulfilled,
+      (state, action: PayloadAction<TCategory>) => {
+        state.loading = "succeed";
+        state.records = action.payload;
+      }
+    ); // fulfilled case
+    builder.addCase(actGetProductsByCatPrefix.rejected, (state, action) => {
+      state.loading = "failed";
+      // set guard to ensure payload is string or null
+
+      if (action.payload && typeof action.payload === "string") {
+        state.error = action.payload;
+      }
+    });
+  },
+});
+
+export {actGetProductsByCatPrefix};
+
+export default productsSlice.reducer;
